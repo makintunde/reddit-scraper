e3f63e08f9b52425e0bbe09dd54b9765e4e41693
Michael Akintunde <michael.akintunde13@imperial.ac.uk>
2017-08-13 20:37:04 +0100

    Initial commit

---
+++ scraper.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import config
+import praw
+import sys
+import getpass
+from termcolor import colored
+
+if len(sys.argv) < 2:
+    raise Exception('No subreddit name provided')
+
+subreddit_name = sys.argv[1]
+
+password = getpass.getpass()
+
+reddit = praw.Reddit(client_id=config.CLIENT_ID,
+                     client_secret=config.CLIENT_SECRET,
+                     password=password,
+                     user_agent=config.USER_AGENT,
+                     username=config.USERNAME)
+
+subreddit = reddit.subreddit(subreddit_name)
+for submission in subreddit.hot(limit=10):
+    print colored(str(submission.score), 'red'), 
+          colored(submission.title,'blue', attrs=['bold'])
+
+    comments = list(submission.comments)
+    
+    if comments:
+        top_comment = comments[0].body
+        details = []
+        
+        if len(top_comment) > 200:
+            partition = top_comment.partition('.')
+            details.append(str(len(partition[-1].split(' '))) + ' more words') 
+            top_comment = partition[0] + '...'
+
+        if len(comments) > 1:
+            details.append(str(max(0, len(comments) - 1)) + ' more comment')
+            if len(comments) > 2:
+                details[-1] += 's'
+    
+        details = colored(', '.join(details) + '.', attrs=['dark'])
+
+        print top_comment 
+        if details:
+            print details
+    else:
+        print '<No comments>'
+    print ''
